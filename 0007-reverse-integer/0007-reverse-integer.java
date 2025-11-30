a12e94f3628aba07bfa467fd55cc8aacf1447a73
Anamika Ranjan <97833859+anamika3012@users.noreply.github.com>
2025-11-30 11:34:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int revno = 0;
+        int num = x;
+        while(num !=0){
+            int digit = num%10;
+            num = num /10;
+            revno = revno *10 + digit;
+        }
+         if(revno > Integer.MAX_VALUE && revno < Integer.MIN_VALUE){
+            return 0;
+        }
+        return revno;
+    }
+}
